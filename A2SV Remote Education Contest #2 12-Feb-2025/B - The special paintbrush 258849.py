9ff775fa6ddfa0f4dba09c52cba6445b9c4b358b
amanabate <129896984+amanabate@users.noreply.github.com>
2025-02-12 10:05:37 +0300

    Created 'B - The special paintbrush' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 12-Feb-2025/B - The special paintbrush 258849.py
@@ -0,0 +1,23 @@
+# Problem: B - The special paintbrush - https://codeforces.com/gym/586622/problem/B
+
+t = int(input())  
+
+for _ in range(t):
+    n = int(input())  
+    s = input() 
+    
+    first_b = -1  
+    last_b = -1   
+    
+
+    for i in range(n):
+        if s[i] == 'B':
+            first_b = i
+            break
+    
+    for i in range(n-1, -1, -1):
+        if s[i] == 'B':
+            last_b = i
+            break
+    
+    print(last_b - first_b + 1)
