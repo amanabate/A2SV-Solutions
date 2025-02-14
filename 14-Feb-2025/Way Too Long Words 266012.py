2b181e0694eef04a840581e9883b3c1344481c97
amanabate <129896984+amanabate@users.noreply.github.com>
2025-02-14 16:56:20 +0300

    Created 'Way Too Long Words' by A2SV Hub

---
+++ 14-Feb-2025/Way Too Long Words 266012.py
@@ -0,0 +1,14 @@
+# Problem: Way Too Long Words - https://codeforces.com/problemset/problem/71/A
+
+number_input = int(input())
+for _ in range(number_input):
+    word = input()
+    if len(word) > 10:
+        word2 = word[0] + str(len(word) - 2) + word[-1]
+        print(word2)
+    else:
+        print(word)
+
+
+
+
