532ebd05290ddb04fa1b5ed90e977b40f5230e2d
amanabate <129896984+amanabate@users.noreply.github.com>
2025-02-21 23:11:13 +0300

    Created 'Python string-split-and-join' by A2SV Hub

---
+++ 21-Feb-2025/Python string-split-and-join 279921.py
@@ -0,0 +1,10 @@
+# Problem: Python string-split-and-join - https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+
+def split_and_join(line):
+    # write your code here
+    return "-".join(line.split())
+    
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
