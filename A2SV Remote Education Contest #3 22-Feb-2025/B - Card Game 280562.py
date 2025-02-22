4f9169bb67ba244f1365c98f24af8ac6d5026204
amanabate <129896984+amanabate@users.noreply.github.com>
2025-02-22 23:04:35 +0300

    Created 'B - Card Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 22-Feb-2025/B - Card Game 280562.py
@@ -0,0 +1,17 @@
+# Problem: B - Card Game - https://codeforces.com/gym/588094/problem/B
+
+n = int(input())
+cards = list(map(int, input().split()))
+
+cards_list = []
+
+for i in range(n):
+    pair = (cards[i], i + 1)
+    cards_list.append(pair)
+
+cards_list.sort()
+
+for i in range(n // 2):
+    smallest = cards_list[i][1]
+    largest = cards_list[n - i - 1][1]
+    print(smallest, largest)
