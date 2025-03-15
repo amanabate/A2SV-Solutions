771a73559b1fc7312b4a13d184cd70a3d1b5b541
amanabate <129896984+amanabate@users.noreply.github.com>
2025-03-15 23:05:36 +0300

    Created 'C - Robin’s Water Wisdom: Stop the Leaks!' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 15-Mar-2025/C - Robin’s Water Wisdom: Stop the Leaks! 308012.py
@@ -0,0 +1,28 @@
+# Problem: C - Robin’s Water Wisdom: Stop the Leaks! - https://codeforces.com/gym/594356/problem/C
+
+n, A, B = map(int, input().split())
+sizes = list(map(int, input().split()))
+
+S = sum(sizes)
+
+first_hole = (sizes[0] * A) / S
+
+if first_hole >= B:
+    print(0)
+
+else:
+
+    other_holes = sorted(sizes[1:], reverse = True)
+
+    blocks = 0
+
+    for hole in other_holes:
+        S -= hole
+        first_hole = (sizes[0] * A) / S
+        blocks += 1
+        
+
+        if first_hole >= B:
+            print(blocks)
+            break
+    
