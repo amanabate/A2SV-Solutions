b8eb2d2681524f9c783481f5e541f1c81de1de7a
amanabate <129896984+amanabate@users.noreply.github.com>
2025-03-17 23:15:04 +0300

    Created 'B - The Time Heist’s Single-Shot Maneuver' by A2SV Hub

---
+++ A2SV Remote Contest #7 17-Mar-2025/B - The Time Heist’s Single-Shot Maneuver 309787.py
@@ -0,0 +1,37 @@
+# Problem: B - The Time Heist’s Single-Shot Maneuver - https://codeforces.com/gym/596004/problem/B
+
+test_cases = int(input())
+
+for _ in range(test_cases):
+    n, m = map(int, input().split())
+    
+    a = list(map(int, input().split()))
+    
+    b = list(map(int, input().split()))
+    b1 = b[0]  
+    
+    
+    prev = -float('inf')
+    possible = True
+    
+    for i in range(n):
+        choice1 = a[i]
+        choice2 = b1 - a[i]
+        
+       
+        if choice1 >= prev and choice2 >= prev:
+            chosen = min(choice1, choice2)
+
+        elif choice1 >= prev:
+            chosen = choice1
+
+        elif choice2 >= prev:
+            chosen = choice2
+
+        else:
+            possible = False
+            break
+        
+        prev = chosen
+    
+    print("YES" if possible else "NO")
