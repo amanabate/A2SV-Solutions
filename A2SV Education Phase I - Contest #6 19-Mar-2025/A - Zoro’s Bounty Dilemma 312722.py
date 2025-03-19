089493cf6f7e907315b7ad8b81d4227da7a07d06
amanabate <129896984+amanabate@users.noreply.github.com>
2025-03-19 22:30:57 +0300

    Created 'A - Zoro’s Bounty Dilemma' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 19-Mar-2025/A - Zoro’s Bounty Dilemma 312722.py
@@ -0,0 +1,15 @@
+# Problem: A - Zoro’s Bounty Dilemma - https://codeforces.com/gym/594356/problem/A
+
+no_test = int(input())
+
+cases = [input().strip() for i in range(no_test)]
+
+for s in cases:
+    if '<' in s and '>' in s:
+        print('?')
+    elif '<' in s:
+        print('<')
+    elif '>' in s:
+        print('>')
+    else:
+        print('=')
