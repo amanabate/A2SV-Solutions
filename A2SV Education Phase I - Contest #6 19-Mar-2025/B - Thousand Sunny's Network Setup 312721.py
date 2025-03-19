af157b47e1be3c90ba7cf283ed2db49923d7d016
amanabate <129896984+amanabate@users.noreply.github.com>
2025-03-19 22:30:45 +0300

    Created 'B - Thousand Sunny's Network Setup' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 19-Mar-2025/B - Thousand Sunny's Network Setup 312721.py
@@ -0,0 +1,9 @@
+# Problem: B - Thousand Sunny's Network Setup - https://codeforces.com/gym/594356/problem/B
+
+n, k = map(int, input().split())
+
+speed = list(map(int, input().split()))
+
+speed.sort(reverse = True)
+
+print(speed[k - 1])
