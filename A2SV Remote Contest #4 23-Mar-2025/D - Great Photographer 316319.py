2d12554f199afafc64102624f1841c8d47f4f3d0
amanabate <129896984+amanabate@users.noreply.github.com>
2025-03-23 22:56:11 +0300

    Created 'D - Great Photographer' by A2SV Hub

---
+++ A2SV Remote Contest #4 23-Mar-2025/D - Great Photographer 316319.py
@@ -0,0 +1,22 @@
+# Problem: D - Great Photographer - https://codeforces.com/gym/590201/problem/D
+
+n , x = map(int, input().split())
+a = [0] * 1006
+
+for _ in range(n):
+    left, right = map(int,input().split())
+    if left > right:
+        left, right = right, left
+   
+    a[left] += 1
+    a[right + 1] -= 1
+
+temp = 0
+answer = 1001
+
+for i in range(1001):
+    temp += a[i]
+    if temp == n:
+        answer = min(answer, abs(i - x))
+
+print(answer if answer != 1001 else - 1)
