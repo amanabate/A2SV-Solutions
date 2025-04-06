79094e71b157d297302c386abffa0d24a04e7c24
amanabate <129896984+amanabate@users.noreply.github.com>
2025-04-06 23:37:46 +0300

    Created 'Thearte Square' by A2SV Hub

---
+++ 06-Apr-2025/Thearte Square 327563.py
@@ -0,0 +1,13 @@
+# Problem: Thearte Square - https://codeforces.com/problemset/problem/1/A
+
+import math
+
+n, m, a = map(int, input().split())
+
+flagstones_n = math.ceil(n / a)
+flagstones_m = math.ceil(m / a)
+
+
+total_flagstones = flagstones_n * flagstones_m
+
+print(total_flagstones)
