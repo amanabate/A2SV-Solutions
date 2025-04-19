0549d1d334d820ada085c7815ea2e6a87a7ff1d8
amanabate <129896984+amanabate@users.noreply.github.com>
2025-04-19 23:43:50 +0300

    Created 'A - The Rules of the Bus' by A2SV Hub

---
+++ A2SV Remote Education Contest #9 19-Apr-2025/A - The Rules of the Bus 337518.py
@@ -0,0 +1,24 @@
+# Problem: A - The Rules of the Bus - https://codeforces.com/gym/603156/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    occupied = set()
+    
+    if n == 1:
+        print("YES")
+        continue
+    
+    occupied.add(a[0])
+    for i in range(1, n):
+        current_seat = a[i]
+        left = current_seat - 1
+        right = current_seat + 1
+        
+        if (left not in occupied) and (right not in occupied):
+            print("NO")
+            break
+        occupied.add(current_seat)
+    else:
+        print("YES")
