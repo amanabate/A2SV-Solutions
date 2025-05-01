629f514377979d31d4677abb82d1597121ae36a4
amanabate <129896984+amanabate@users.noreply.github.com>
2025-05-01 23:52:38 +0300

    Created 'C - Mr. Boomsy' by A2SV Hub

---
+++ A2SV Contest #11 01-May-2025/C - Mr. Boomsy 346677.py
@@ -0,0 +1,17 @@
+# Problem: C - Mr. Boomsy - https://codeforces.com/gym/605795/problem/C
+
+t = int(input())
+
+for _ in range(t):
+    user_input = str(input())
+
+    stack = []
+
+    for ch in user_input:
+        if stack and ch == 'B':
+              stack.pop()
+        else:
+           stack.append(ch)
+
+    print(len(stack))
+    
