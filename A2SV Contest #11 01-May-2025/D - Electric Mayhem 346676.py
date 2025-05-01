9fb285f5eb1090de8a7ce8a50ae1f46668cce8d2
amanabate <129896984+amanabate@users.noreply.github.com>
2025-05-01 23:52:07 +0300

    Created 'D - Electric Mayhem' by A2SV Hub

---
+++ A2SV Contest #11 01-May-2025/D - Electric Mayhem 346676.py
@@ -0,0 +1,16 @@
+# Problem: D - Electric Mayhem - https://codeforces.com/gym/605795/problem/D
+
+user_input = input().strip()
+
+stack = []
+
+for char in user_input:
+    if stack and stack[-1] == char:
+        stack.pop()
+    else:
+        stack.append(char)
+    
+if not stack:
+    print('Yes')
+else:
+    print('No')
