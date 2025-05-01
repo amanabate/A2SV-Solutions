2591225706b251b2a18e71c1548166c33cc7321d
amanabate <129896984+amanabate@users.noreply.github.com>
2025-05-02 00:02:00 +0300

    Created 'B - Aaron and Repeated Letters' by A2SV Hub

---
+++ A2SV Contest #11 01-May-2025/B - Aaron and Repeated Letters 346692.py
@@ -0,0 +1,12 @@
+# Problem: B - Aaron and Repeated Letters - https://codeforces.com/gym/605795/problem/B
+
+user_input = str(input())
+
+stack = []
+
+for ch in user_input:
+    if stack and stack[-1] == ch:
+        stack.pop()
+    else:
+        stack.append(ch)
+print(''.join(stack))
