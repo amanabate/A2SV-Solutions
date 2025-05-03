72ac406e2a24b7e7516d8a088f14c108473b0717
amanabate <129896984+amanabate@users.noreply.github.com>
2025-05-03 22:35:28 +0300

    Created 'A - Modern Name Generator' by A2SV Hub

---
+++ A2SV Contest #11 03-May-2025/A - Modern Name Generator 347822.py
@@ -0,0 +1,14 @@
+# Problem: A - Modern Name Generator - https://codeforces.com/gym/605795/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    user_input = list(input().split())
+
+    result = []
+
+    for word in user_input:
+        result.append(word[0])
+    print(''.join(result))
+
+
