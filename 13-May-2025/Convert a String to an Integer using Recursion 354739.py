aa0b718483e7a8010e433020c97c1b7e18dddcfb
amanabate <129896984+amanabate@users.noreply.github.com>
2025-05-13 21:40:16 +0300

    Created 'Convert a String to an Integer using Recursion' by A2SV Hub

---
+++ 13-May-2025/Convert a String to an Integer using Recursion 354739.py
@@ -0,0 +1,13 @@
+# Problem: Convert a String to an Integer using Recursion - https://www.geeksforgeeks.org/convert-a-string-to-an-integer-using-recursion/
+
+def stringToIntHelper(str, index):
+    if index == len(str):
+        return 0
+
+    digit = int(str[index])
+
+    return digit * (10 ** (len(str) - index - 1)) + stringToIntHelper(str, index + 1)
+
+
+def stringToInt(str):
+    return stringToIntHelper(str, 0)
