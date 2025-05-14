314e6eda551679d5b89dd8763db9b9c20b1882d8
amanabate <129896984+amanabate@users.noreply.github.com>
2025-05-14 23:01:09 +0300

    Created 'Arranging Coins' by A2SV Hub

---
+++ 14-May-2025/Arranging Coins 355887.py
@@ -0,0 +1,18 @@
+# Problem: Arranging Coins - https://leetcode.com/problems/arranging-coins/description/
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+
+        low = 0
+        high = n
+        
+        while low <= high:
+            mid = low + (high - low) // 2
+            total = mid * (mid + 1) // 2
+            if total == n:
+                return mid
+            elif total < n:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return high
