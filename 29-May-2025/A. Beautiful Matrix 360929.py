8cbb123775256f79da6c5c92824e9aaf282f3295
amanabate <129896984+amanabate@users.noreply.github.com>
2025-05-29 21:27:46 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 29-May-2025/A. Beautiful Matrix 360929.py
@@ -0,0 +1,14 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+
+matrix = []
+
+for i in range(5):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+    if 1 in row:
+        one_row = i + 1      
+        one_col = row.index(1) + 1
+
+moves = abs(one_row - 3) + abs(one_col - 3)
+print(moves)
