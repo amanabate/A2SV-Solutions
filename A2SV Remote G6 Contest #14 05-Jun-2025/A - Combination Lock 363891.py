8c38d4c235ffc8d8b46557cc26dfa82893dd6de5
amanabate <129896984+amanabate@users.noreply.github.com>
2025-06-05 16:01:57 +0300

    Created 'A - Combination Lock' by A2SV Hub

---
+++ A2SV Remote G6 Contest #14 05-Jun-2025/A - Combination Lock 363891.py
@@ -0,0 +1,23 @@
+# Problem: A - Combination Lock - https://codeforces.com/gym/610550/problem/A
+
+n = int(input())
+original_state = input()
+target = input()
+
+
+dict = {}
+
+for i in range(10):
+    for j in range(10):
+        forward = abs(i - j)
+        backward = 10 - forward
+        dict[(i, j)] = min(forward, backward)
+
+
+moves = 0
+for i in range(n):
+    from_digit = int(original_state[i])
+    to_digit = int(target[i])
+    moves += dict[(from_digit, to_digit)]
+
+print(moves)
