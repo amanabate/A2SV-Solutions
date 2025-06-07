678a156d6ff205f7691d1b1171c6b02bba993d3e
amanabate <129896984+amanabate@users.noreply.github.com>
2025-06-07 23:26:36 +0300

    Created 'Permutations II' by A2SV Hub

---
+++ 07-Jun-2025/Permutations II 364628.py
@@ -0,0 +1,26 @@
+# Problem: Permutations II - https://leetcode.com/problems/permutations-ii/description/
+
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
+        res = []
+        counter = Counter(nums)
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+
+            for num in counter:
+                if counter[num] > 0:
+                    path.append(num)
+                    counter[num] -= 1
+
+                    backtrack(path)
+
+                    path.pop()
+                    counter[num] += 1
+
+        backtrack([])
+        return res
