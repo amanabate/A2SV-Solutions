e5188aa7acf8c9e9b9c14a9891ef06bb2f8e3c15
amanabate <129896984+amanabate@users.noreply.github.com>
2025-06-10 00:17:59 +0300

    Created 'C - Number Mutation' by A2SV Hub

---
+++ A2SV Remote contest #17 09-Jun-2025/C - Number Mutation 365238.py
@@ -0,0 +1,33 @@
+# Problem: C - Number Mutation - https://codeforces.com/gym/614464/problem/C
+
+a, b = map(int, input().split())
+
+def find_path(a, b):
+
+    path = []
+    current = b
+    path.append(current)
+
+    while current > a:
+        if current % 2 == 0:
+            current = current // 2
+            path.append(current)
+            
+        elif current % 10 == 1:
+            current = current // 10
+            path.append(current)
+        else:
+            return 
+    if current == a:
+        path.reverse()
+        return path
+    else:
+        return 
+
+path = find_path(a, b)
+if path:
+    print("YES")
+    print(len(path))
+    print(' '.join(map(str, path)))
+else:
+    print("NO")
