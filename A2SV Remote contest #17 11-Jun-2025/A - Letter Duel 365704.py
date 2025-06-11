fac4b6f3ff9f04ce34c348c532abff3dace5f462
amanabate <129896984+amanabate@users.noreply.github.com>
2025-06-11 22:29:54 +0300

    Created 'A - Letter Duel' by A2SV Hub

---
+++ A2SV Remote contest #17 11-Jun-2025/A - Letter Duel 365704.py
@@ -0,0 +1,24 @@
+# Problem: A - Letter Duel - https://codeforces.com/gym/614464/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input().strip()
+    start, end = -1, -1
+   
+    for i in range(n - 1):
+        if s[i] != s[i + 1]:
+            start, end = i + 1, i + 2
+            break
+
+    if start != -1:
+        print(start, end)
+    else:
+        count_a = s.count('a')
+        count_b = s.count('b')
+
+        if count_a == count_b and n >= 2:
+            print(1, n)
+        else:
+            print(-1, -1)
+
