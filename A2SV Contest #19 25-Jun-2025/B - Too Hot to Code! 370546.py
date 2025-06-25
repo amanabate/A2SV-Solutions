2cafb46e972e7aa97ddf7fbc5eea09f1eed205e9
amanabate <129896984+amanabate@users.noreply.github.com>
2025-06-25 23:12:16 +0300

    Created 'B - Too Hot to Code!' by A2SV Hub

---
+++ A2SV Contest #19 25-Jun-2025/B - Too Hot to Code! 370546.py
@@ -0,0 +1,23 @@
+# Problem: B - Too Hot to Code! - https://codeforces.com/gym/617023/problem/B
+
+import sys
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    n, k = map(int, sys.stdin.readline().split())
+    a = list(map(int, sys.stdin.readline().split()))
+
+    a.sort()
+
+    max_len = 1
+    current_len = 1
+
+    for i in range(1, n):
+        if a[i] - a[i-1] <= k:
+            current_len += 1
+            max_len = max(max_len, current_len)
+
+        else:
+            current_len = 1
+            
+    print(n - max_len)
