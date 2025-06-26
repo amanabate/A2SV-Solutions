dfc4f2548070a4a9831d263310ca5ac5929aefdf
amanabate <129896984+amanabate@users.noreply.github.com>
2025-06-26 22:05:21 +0300

    Created 'A - Student Rankings by Ratings' by A2SV Hub

---
+++ A2SV Contest #19 26-Jun-2025/A - Student Rankings by Ratings 370865.py
@@ -0,0 +1,22 @@
+# Problem: A - Student Rankings by Ratings - https://codeforces.com/gym/617023/problem/A
+
+n = int(input())
+
+rates = list(map(int, input().split()))
+
+positions = []
+
+for i in range(n):
+    current_rate = rates[i]
+
+    count = 0
+
+    for r in rates:
+        if r > current_rate:
+            count += 1
+
+    position = count + 1
+
+    positions.append(position)
+
+print(' '.join(map(str,positions)))
