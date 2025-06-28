76f64d0bba25923aeeeb038cc8475c2abb401a63
amanabate <129896984+amanabate@users.noreply.github.com>
2025-06-28 23:04:26 +0300

    Created 'A - Card Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #18 28-Jun-2025/A - Card Game 371493.py
@@ -0,0 +1,12 @@
+# Problem: A - Card Game - https://codeforces.com/gym/616066/problem/A
+
+t = int(input())
+for _ in range(t):
+    n, k1, k2 = map(int, input().split())
+    a = list(map(int, input().split()))  # First player's cards
+    b = list(map(int, input().split()))  # Second player's cards
+
+    if max(a) > max(b):
+        print("YES")
+    else:
+        print("NO")
