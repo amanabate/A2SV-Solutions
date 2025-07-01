13eee1cce3c37193541594e36c5066a9f27a57e7
amanabate <129896984+amanabate@users.noreply.github.com>
2025-07-01 21:58:46 +0300

    Created 'A - Coins' by A2SV Hub

---
+++ A2SV Contest #20 01-Jul-2025/A - Coins 372525.py
@@ -0,0 +1,13 @@
+# Problem: A - Coins - https://codeforces.com/gym/618729/problem/A
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    
+    if n % 2 == 0:
+        print("YES")
+    else:
+        if k % 2 == 1 and k <= n:
+            print("YES")
+        else:
+            print("NO")
