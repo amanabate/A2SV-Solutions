279847334a516b5d12484bf782c98444091ed7da
amanabate <129896984+amanabate@users.noreply.github.com>
2025-07-02 22:06:34 +0300

    Created 'B - New Year Transportation' by A2SV Hub

---
+++ A2SV Contest #20 02-Jul-2025/B - New Year Transportation 372762.py
@@ -0,0 +1,13 @@
+# Problem: B - New Year Transportation - https://codeforces.com/gym/618729/problem/B
+
+n, t = map(int, input().split())
+a = list(map(int, input().split()))
+
+curr_pos = 1
+while curr_pos < t:
+    curr_pos += a[curr_pos - 1]
+
+if curr_pos == t:
+    print("YES")
+else:
+    print("NO")
