1e11565ec4a1d5e90131f7bc0018c6b4fb7277ea
amanabate <129896984+amanabate@users.noreply.github.com>
2025-07-07 22:32:21 +0300

    Created 'A - Twice' by A2SV Hub

---
+++ A2SV Contest #21 07-Jul-2025/A - Twice 374373.py
@@ -0,0 +1,20 @@
+# Problem: A - Twice - https://codeforces.com/gym/619446/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    freq = {}
+    for num in a:
+        freq[num] = freq.get(num, 0) + 1
+
+    score = 0
+    for count in freq.values():
+        
+        while count >= 2:
+            score += 1
+            count -= 2
+
+    print(score)
