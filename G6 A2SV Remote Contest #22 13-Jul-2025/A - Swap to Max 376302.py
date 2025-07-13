114570e147b18be19a6a8379f308f14bd9cf976d
amanabate <129896984+amanabate@users.noreply.github.com>
2025-07-13 23:25:05 +0300

    Created 'A - Swap to Max' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 13-Jul-2025/A - Swap to Max 376302.py
@@ -0,0 +1,21 @@
+# Problem: A - Swap to Max - https://codeforces.com/gym/622136/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    last_a = []
+    last_b = []
+
+    for i in range(n):
+        if a[i] > b[i]:
+            last_a.append(b[i])
+            last_b.append(a[i])
+        else:
+            last_a.append(a[i])
+            last_b.append(b[i])
+    
+    print(max(last_a) * max(last_b))
