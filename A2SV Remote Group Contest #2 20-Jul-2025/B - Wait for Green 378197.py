77b7fb41ca5bf80c3bb96795fdda0430be4b5ce4
amanabate <129896984+amanabate@users.noreply.github.com>
2025-07-20 22:01:27 +0300

    Created 'B - Wait for Green' by A2SV Hub

---
+++ A2SV Remote Group Contest #2 20-Jul-2025/B - Wait for Green 378197.py
@@ -0,0 +1,29 @@
+# Problem: B - Wait for Green - https://codeforces.com/gym/623571/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, c = input().split()
+    n = int(n)
+    s = input()
+    
+    if c == 'g':
+        print(0)
+        continue
+    
+    wait_times = []
+    
+    green_pos = [i for i in range(n) if s[i] == 'g']
+    
+    max_wait = 0
+    for i in range(n):
+        if s[i] == c:
+           
+            for g in green_pos:
+                if g >= i:
+                    wait = g - i
+                    break
+            else:
+                wait = (green_pos[0] + n) - i
+            max_wait = max(max_wait, wait)
+    
+    print(max_wait)
