36f3467254610c3e83cad115aca1fc95f427b107
amanabate <129896984+amanabate@users.noreply.github.com>
2025-07-28 20:52:58 +0300

    Created 'A - Verifying Access Keys' by A2SV Hub

---
+++ A2SV Remote Education Contest #23 28-Jul-2025/A - Verifying Access Keys 380031.py
@@ -0,0 +1,24 @@
+# Problem: A - Verifying Access Keys - https://codeforces.com/gym/625306/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+
+    
+    first_letter_index = n
+
+    for i in range(n):
+        if s[i].isalpha():
+            first_letter_index = i
+            break
+
+    
+    digits = s[:first_letter_index]
+    letters = s[first_letter_index:]
+
+   
+    if digits == ''.join(sorted(digits)) and letters == ''.join(sorted(letters)):
+        print("YES")
+    else:
+        print("NO")
