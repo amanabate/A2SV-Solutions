f6803b6db1aa908db11b249b49acb4fd5253ac0c
amanabate <129896984+amanabate@users.noreply.github.com>
2025-07-28 21:52:22 +0300

    Created 'Palindrome Number' by A2SV Hub

---
+++ 28-Jul-2025/Palindrome Number 380068.py
@@ -0,0 +1,14 @@
+# Problem: Palindrome Number - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        s = str(x)
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
