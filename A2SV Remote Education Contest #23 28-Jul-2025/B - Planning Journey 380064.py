183f662c5a5e2d7beecf65b04e12fae83029d03b
amanabate <129896984+amanabate@users.noreply.github.com>
2025-07-28 21:48:02 +0300

    Created 'B - Planning Journey' by A2SV Hub

---
+++ A2SV Remote Education Contest #23 28-Jul-2025/B - Planning Journey 380064.py
@@ -0,0 +1,23 @@
+# Problem: B - Planning Journey - https://codeforces.com/gym/625306/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    
+    i = 0
+    journeys = 0
+    current_clear = 0
+
+    while i < n:
+        if a[i] == 0:
+            current_clear += 1
+            if current_clear == k:
+                journeys += 1
+                current_clear = 0
+                i += 1  # skip rest day
+        else:
+            current_clear = 0
+        i += 1
+
+    print(journeys)
