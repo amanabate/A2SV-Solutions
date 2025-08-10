9fd84bda929d46dab03caf94f96b8fea0f869bc3
amanabate <129896984+amanabate@users.noreply.github.com>
2025-08-10 22:59:07 +0300

    Created 'Is Subsequence' by A2SV Hub

---
+++ 10-Aug-2025/Is Subsequence 383407.py
@@ -0,0 +1,9 @@
+# Problem: Is Subsequence - https://leetcode.com/problems/is-subsequence/
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = 0  # pointer for s
+        for char in t:
+          if i < len(s) and s[i] == char:
+            i += 1
+        return i == len(s)
