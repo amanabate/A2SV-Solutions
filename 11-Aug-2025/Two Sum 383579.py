118e3498e51403ca0ab78a33a42972929567209f
amanabate <129896984+amanabate@users.noreply.github.com>
2025-08-11 20:54:18 +0300

    Created 'Two Sum' by A2SV Hub

---
+++ 11-Aug-2025/Two Sum 383579.py
@@ -0,0 +1,10 @@
+# Problem: Two Sum - https://leetcode.com/problems/two-sum/description
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in seen:
+                return [seen[diff], i]
+            seen[num] = i
