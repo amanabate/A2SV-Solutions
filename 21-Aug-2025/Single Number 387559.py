f0852c34e0991367e1d28c33e42be5daa0f809c9
amanabate <129896984+amanabate@users.noreply.github.com>
2025-08-21 21:22:21 +0300

    Created 'Single Number' by A2SV Hub

---
+++ 21-Aug-2025/Single Number 387559.py
@@ -0,0 +1,8 @@
+# Problem: Single Number - https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for n in nums:
+            ans ^= n   # XOR cancels out duplicates
+        return ans
