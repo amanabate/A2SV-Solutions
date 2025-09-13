d36899a952b98330d01ca2fce1f0cae4e50438b0
amanabate <129896984+amanabate@users.noreply.github.com>
2025-09-13 21:03:10 +0300

    Created 'Three Divisors' by A2SV Hub

---
+++ 13-Sep-2025/Three Divisors 392989.py
@@ -0,0 +1,14 @@
+# Problem: Three Divisors - https://leetcode.com/problems/three-divisors/description/?envType=problem-list-v2&envId=number-theory
+
+class Solution:
+    def isThree(self, n: int) -> bool:
+        def isPrime(x):
+            if x < 2:
+                return False
+            for i in range(2, int(x**0.5) + 1):
+                if x % i == 0:
+                    return False
+            return True
+        
+        root = int(n**0.5)
+        return root * root == n and isPrime(root)
