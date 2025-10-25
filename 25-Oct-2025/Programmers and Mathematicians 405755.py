2e5369d33dfee82a886df4cb166e470d995f939a
Amanuel Abate <129896984+amanabate@users.noreply.github.com>
2025-10-26 00:05:07 +0300

    Created 'Programmers and Mathematicians' by A2SV Hub

---
+++ 25-Oct-2025/Programmers and Mathematicians 405755.py
@@ -0,0 +1,6 @@
+# Problem: Programmers and Mathematicians - https://codeforces.com/problemset/problem/1611/B
+
+t = int(input())
+for _ in range(t):
+    a, b = map(int, input().split())
+    print(min((a + b) // 4, min(a, b)))
